f47f3f8b87f34f64ae1289f3423649f3e5dedd83
Claus Polanka <claus.polanka@chello.at>
2015-03-12 11:01:59 +0100

    [Red] Level 1 spec example 1.

---
+++ PicturePuzzle.Test/PicturePuzzleAcceptanceTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PicturePuzzle.Test
+{
+    [TestFixture]
+    public class PicturePuzzleAcceptanceTests
+    {
+        private const char BLANK = ' ';
+
+        [TestCase("5 4", "??1??")]
+        public void Level_1_spec_examples(string input, string expected)
+        {
+            var cols = input.Split(BLANK)[0];
+            var blockLength = input.Split(BLANK)[1];
+            var actual = FillCells(cols, blockLength);
+            Assert.That(actual, Is.EqualTo(expected), "cells");
+        }
+
+        private string FillCells(string cols, string blockLength)
+        {
+            return string.Empty;
+        }
+    }
+}
